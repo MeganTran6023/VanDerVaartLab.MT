829f6f2f101df011eb09671a890d5dd85c610ba1
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2023-09-03 13:29:09 -0400

    Update 9.1.py

--- 9.1.py
+++ 9.1.py
@@ -49,9 +49,11 @@ inpcrd = AmberInpcrdFile('noe-%s.rst' % sequence) #MT - input coordinate file (a
 
 #6) MT- 9/2
 print ('Setting up system...')
-system = prmtop.createSystem(nonbondedMethod=PME,
-    nonbondedCutoff=cutoff*unit.nanometers, constraints=HBonds, rigidWater=True,
-    ewaldErrorTolerance=0.00005)
+system = prmtop.createSystem(nonbondedMethod=PME, #MT- uses Particle Mesh Ewald (PME) to calculate electrostatic interactions
+    nonbondedCutoff=cutoff*unit.nanometers, #MT- program doesnt account for bonds greater than the cutoff value (length), allows GPU to save space to focus on other parts of setting up simulation
+    constraints=HBonds, #maintains Hydrogen bonds
+    rigidWater=True, #maintains distances and angles within water molecules. Allows for focusing on solute of interest and reduces computational cost
+    ewaldErrorTolerance=0.00005) #MT- error tolerance for  Ewald summation method, lower number  = more accurate, but uses more memory
 
 print('Choosing integrator...') #MT - 7) integrator solves equations of motions to setup positions and velocities of particles over time
 integrator = LangevinIntegrator(temp*unit.kelvin, gamma/unit.picoseconds,
