2548731bc482cd61c29ed0e1f6dfcdd2398e2b5b
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2023-09-13 20:33:56 -0400

    Create 9.13_RotationUracil.py

---
+++ 9.13_RotationUracil.py
@@ -0,0 +1,8 @@
+#MT 9/13 - was assigned to create a script to flip x,y,and z coordinates of a uracil base in a DNA strand.
+
+#Outline:
+
+#1) setlect all atom coordinates from input .dat file
+#2) Find axis of symmetry to flip 180 degrees over
+#3) Form transformation matrix, make vector negative
+#4) output new .dat file
