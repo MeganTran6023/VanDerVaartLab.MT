084884672744c27dba0c0e22942f5fccdfc10279
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2023-09-23 11:24:41 -0400

    Create 9_22.MakeAnBDNA.sh

---
+++ 9_22.MakeAnBDNA.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Seperate a double stranded DNA to A and B strand
+
+#1- Set input pdb file
+input_pdb="practice_out.pdb"
+
+#2- Make seperate files for chain A and B
+#will output chain_A.pdb and chain_B.pdb
+
+grep '^ATOM.* A ' "input_pdb" > chain_A.pdb
+grep '^ATOM.* B ' "input_pdb" > chain_B.pdb
+
+#use sh to run
