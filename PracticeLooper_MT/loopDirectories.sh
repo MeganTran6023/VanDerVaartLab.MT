4e7d9cc090f764c16425aeefdc51ff9ca09c65da
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2023-10-29 10:20:49 -0400

    Add files via upload

--- PracticeLooper_MT/loopDirectories.sh
+++ PracticeLooper_MT/loopDirectories.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-directories="FileA FileB FileC"
+directories=("FileA" "FileB" "FileC")
 
-for i in $directories; do
+for i in "${directories[@]}"; do
+    # Go into each directory
+    cd "$i" || { echo "Could not change directory to $i"; exit 1; }
 
-#go into each directory
-	cd "$i" || { echo "Could not change directory to $i"; exit 1; }
-
-# Make practice .out file in each directory
-
-	cat <<EOF > practice.out
-
+    # Make a practice .out file in each directory
+    cat <<EOF > practice.out
 This is a practice .out file.
 EOF
 
-	# Optional: Print the current working directory
-	pwd
+    # Optional: Print the current working directory
+    echo "Current directory: $(pwd)"
 
-	#Go to original directory
-	cd ..
+    # Check to see if "complete" is present (if-else statement)
+    if grep -q 'complete' *; then
+        echo "$i is complete"
+    else
+        echo "$i is incomplete"
+    fi
+
+    # Go back to the original directory
+    cd ..
 done
-
-
