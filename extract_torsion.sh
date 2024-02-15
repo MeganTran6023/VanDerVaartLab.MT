2ec1c51dbe78bd866505720181a50031dd2d744e
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2024-02-15 11:24:38 -0500

    Update extract_torsion.sh

--- extract_torsion.sh
+++ extract_torsion.sh
@@ -2,34 +2,39 @@
 
 # Set the current working directory as the project directory
 path=$(pwd)
-
-# Define directories and paths
 base_path="/work/m/megantran"
-subdirectories_major=("GTCC CTCT GTCT TTCC CTCC TTCT ATCT")
-subdirectories_minor=("CTCT ATCT")
 torsion_names="delta zeta beta alpha gamma epsilon chi"
 
-# Create directories for each torsion name
+# List all subdirectories in the base directory - run in GC_XTUY - work directory - these are register shifted of interest
+subdirectories_major=("GTCC" "CTCT" "GTCT" "TTCC" "CTCC" "TTCT" "ATCT")
+subdirectories_minor=("CTCT" "ATCT")
+
 for item in $torsion_names; do
     mkdir "${item}_torsion"
 done
 
-# Loop through each subdirectory, find torsion dat files for each torsion name
+# Loop through each subdirectory for majors, find torsion dat files for each greek name 
 for sub_dir in "${subdirectories_major[@]}"; do
-    for dir in ${subdirectories_minor[@]}; do
-        for item in $torsion_names; do
-            # Enter into the directory
-            subdir_path="$sub_dir/prod/$dir/an/backbone/torsion/"
-            cd "$subdir_path" || exit
+    for item in $torsion_names; do
+        # Enter into directory
+        subdir_path="$sub_dir/prod/major/an/backbone/torsion/"
+        cd "$subdir_path" || exit
+        # Get respective .dat files - each greek letter own file
+        echo "$item"*.dat
+        cp "$item"*.dat "$base_path"/XTUY_GC/XTUY/"${item}_torsion"
+        cd "$path" || exit
+    done
+done
 
-            # List the respective .dat files for each torsion name
-            echo "$item"*.dat
-
-            # Copy the .dat files to the specified directory
-            cp "$item"*.dat "$base_path/XTUY_GC/XTUY/${item}_torsion"
-
-            # Return to the original directory
-            cd "$path" || exit
-        done
+# Loop through each subdirectory for minors, find torsion dat files for each greek name 
+for sub_dir in "${subdirectories_minor[@]}"; do
+    for item in $torsion_names; do
+        # Enter into directory
+        subdir_path="$sub_dir/prod/minor/an/backbone/torsion/"
+        cd "$subdir_path" || exit
+        # Get respective .dat files - each greek letter own file
+        echo "$item"*.dat has been transferred into "${item}_torsion"
+        cp "$item"*.dat "$base_path"/XTUY_GC/XTUY/"${item}_torsion"
+        cd "$path" || exit
     done
 done
