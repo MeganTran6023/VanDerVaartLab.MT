6bba43ad16e78ebd227298ba3172f47e61a2d49e
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2024-02-15 10:57:03 -0500

    Update extract_torsion.sh

--- extract_torsion.sh
+++ extract_torsion.sh
@@ -1,39 +1,35 @@
 #!/bin/bash
 
 # Set the current working directory as the project directory
+path=$(pwd)
 
-subd="in aj"
-
+# Define directories and paths
 base_path="/work/m/megantran"
-
+subdirectories_major=("GTCC CTCT GTCT TTCC CTCC TTCT ATCT")
+subdirectories_minor=("CTCT ATCT")
 torsion_names="delta zeta beta alpha gamma epsilon chi"
 
-
-# List all subdirectories in the base directory - run in GC_XTUY - work directory - these are register shifted of interest
-subdirectories_major=("GTCC CTCT GTCT TTCC CTCC TTCT ATCT" )
-subdirectories_minor=("CTCT ATCT")
-
-
+# Create directories for each torsion name
 for item in $torsion_names; do
-    mkdir "$item"_torsion
+    mkdir "${item}_torsion"
 done
 
+# Loop through each subdirectory, find torsion dat files for each torsion name
+for sub_dir in "${subdirectories_major[@]}"; do
+    for dir in ${subdirectories_minor[@]}; do
+        for item in $torsion_names; do
+            # Enter into the directory
+            subdir_path="$sub_dir/prod/$dir/an/backbone/torsion/"
+            cd "$subdir_path" || exit
 
-# Loop through each subdirectory, find torsion dat files for each greek name 
-for sub_dir in "${subdirectories[@]}"; do
-        for dir in $subd; do
-            for item in $torsion_names; do
+            # List the respective .dat files for each torsion name
+            echo "$item"*.dat
 
-                #enter in directory
-                 subdir_path="$sub_dir/prod/$dir/an/backbone/torsion/"
-                 cd $subdir_path
-                #get respective .dat files - each greeek letter own file
-                 cp $item*.dat "$base_path"/XTUY_GC/XTUY/"$item_torsion"
+            # Copy the .dat files to the specified directory
+            cp "$item"*.dat "$base_path/XTUY_GC/XTUY/${item}_torsion"
 
-                 done
+            # Return to the original directory
+            cd "$path" || exit
         done
+    done
 done
-
-                 
-
-                
